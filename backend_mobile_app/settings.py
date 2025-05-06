09cd226eff79245d13aa325231e9ccc66ea00e12
umarkhemis <umarkhemis9@gmail.com>
2025-05-06 11:11:16 +0300

    Modified the backend code

--- backend_mobile_app/settings.py
+++ backend_mobile_app/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -149,9 +149,16 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "static/"
+# STATIC_URL = '/static/'
+
+# Whitenoise
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-GEMINI_API_KEY = 'AIzaSyB9IHWHbqggP__-hN9304vrJqTnvTDha3c'
