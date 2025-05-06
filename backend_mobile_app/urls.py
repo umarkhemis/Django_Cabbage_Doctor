84caa7e500940c82a30dbb57d3bc6d755aa18a3e
umarkhemis <umarkhemis9@gmail.com>
2025-05-06 17:03:14 +0300

    modified the backend code

--- backend_mobile_app/urls.py
+++ backend_mobile_app/urls.py
@@ -19,8 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from cabbage_black_rot import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('/', include('cabbage_black_rot.urls')),
+    path('', views.get_history, name='history'),
+    path('api/', include('cabbage_black_rot.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
